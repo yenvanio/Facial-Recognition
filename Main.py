cce88e7cf0decb9513b8d427414b07b2c03ebdf8
Shivanouyen Yoganathan <shivanouyen.yoganathan@uoit.net>
2017-03-12 16:27:46 -0400

    YCbCr
    Converted to three channels, plotting correlation between Cb an Cr and
    removing Y to get the most likely to be skin areas

--- Main.py
+++ Main.py
@@ -1,5 +1,37 @@
-import cv2
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+import copy
 
-im = cv2.imread("exercise-1.jpg")
+
+img = Image.open("exercise-4.jpg")
+img = img.convert('YCbCr')
+ycbcr = np.array(img)
+
+Y = 0
+Cb = 1
+Cr = 2
+
+YCbCr = list(img.getdata())
+imYCbCr = np.reshape(YCbCr, (img.size[1], img.size[0], 3))
+imYCbCr = imYCbCr.astype(np.uint8)
+
+yChannel = Image.fromarray(imYCbCr[:,:,Y], "L") #L is for 8-bit pixels (mode)
+cbChannel = Image.fromarray(imYCbCr[:,:,Cb], "L")
+crChannel = Image.fromarray(imYCbCr[:,:,Cr], "L")
+
+plt.imshow(cbChannel)
+plt.imshow(crChannel)
+plt.show()
+
+#Show images with effect of each channel
+# yChannel.show()
+# cbChannel.show()
+# crChannel.show()
+
+
+
+
+
 
 
