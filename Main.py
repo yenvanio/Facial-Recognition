2a3a42921b2151de59b288e246ed4edb6338c7fd
Shivanouyen Yoganathan <shivanouyen.yoganathan@uoit.net>
2017-03-12 17:31:16 -0400

    Binary Image
    Converted Image to Black and White with the use of cb and cr channels
    and generated the areas that have skin as white and no skin as black

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
+import cv2
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
-import copy
 
-
+src = cv2.imread("exercise-4.jpg")
 img = Image.open("exercise-4.jpg")
+res = src.copy()
 img = img.convert('YCbCr')
 ycbcr = np.array(img)
 
@@ -20,14 +21,41 @@ yChannel = Image.fromarray(imYCbCr[:,:,Y], "L") #L is for 8-bit pixels (mode)
 cbChannel = Image.fromarray(imYCbCr[:,:,Cb], "L")
 crChannel = Image.fromarray(imYCbCr[:,:,Cr], "L")
 
-plt.imshow(cbChannel)
-plt.imshow(crChannel)
+# arrCB = np.asarray(cbChannel)
+# arrCR = np.asarray(crChannel)
+# plt.hist(arrCB)
+# plt.show()
+# plt.hist(arrCR)
+# plt.show()
+
+white = [255,255,255]
+black = [0,0,0]
+
+for (x,y), pixel in np.ndenumerate(cbChannel):
+    if( pixel >=105 and pixel <= 135 ):
+        res[x,y,:] = white
+    else:
+        res[x,y,:] = black
+
+for (x,y), pixel in np.ndenumerate(crChannel):
+    if( pixel >=140 and pixel <= 165 ):
+        res[x,y,:] = white
+    else:
+        res[x,y,:] = black
+
+
+
+plt.imshow(res)
 plt.show()
+
 
 #Show images with effect of each channel
 # yChannel.show()
 # cbChannel.show()
 # crChannel.show()
+
+
+
 
 
 
